a4492f4d96a4ce1436a504aa6a0ac6007b03f360
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-07-04 13:40:57 +0530

    add enum

---
+++ Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Enum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Customer[] customer = new Customer[3];
+            customer[0] = new Customer { Name = "John", gender = Gender.Male };
+            customer[1] = new Customer { Name = "Marry", gender = Gender.Female };
+            customer[2] = new Customer { Name = "Unknown Guy", gender = Gender.Unknown };
+            foreach (Customer cus in customer)
+            {
+                System.Console.WriteLine(cus.gender);
+            }
+        }
+    }
+    public enum Gender
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = 2
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public Gender gender { get; set; }
+    }
+}
